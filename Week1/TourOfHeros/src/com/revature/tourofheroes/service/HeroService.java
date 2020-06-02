edbc16e486452484098217ac737a77d274486f9e
reuhdz <30872054+reuhdz@users.noreply.github.com>
2020-06-02 16:55:48 -0400

    User input added to MainMenu class

---
+++ Week1/TourOfHeros/src/com/revature/tourofheroes/service/HeroService.java
@@ -0,0 +1,28 @@
+package com.revature.tourofheroes.service;
+
+import com.revature.tourofheros.exceptions.InvalidHealthValueException;
+import com.revature.tourofheros.models.Hero;
+
+public class HeroService {
+	public boolean createHero() {
+		String[] specialMoves = {"heat vision", "light", "invulnerability"};
+		
+		Hero newHero;
+		//order matters in catch blocks
+		try {
+			newHero = new Hero("Superman", specialMoves, -1, false);
+		} catch (InvalidHealthValueException e){
+			e.printStackTrace();
+			return false;
+		} finally {
+			//finally runs always
+			//usually where clean up goes (file, connection close etc.) 
+			System.out.println("Tried to make a hero could've failed");
+		}
+		
+		//try with resources - to read on
+		
+		System.out.println(newHero);
+		return true;
+	}
+}
---
+++ Week1/TourOfHeros/src/com/revature/tourofheros/exceptions/InvalidHealthValueException.java
@@ -0,0 +1,13 @@
+package com.revature.tourofheros.exceptions;
+
+public class InvalidHealthValueException extends Exception {
+	
+	
+	private static final long serialVersionUID = -3302351326146231865L;
+	public InvalidHealthValueException() {
+		super("Health level should not be negative!");
+	}
+	public InvalidHealthValueException(String message) {
+		super(message);
+	}
+}
