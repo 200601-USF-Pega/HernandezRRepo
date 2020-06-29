59d301d31da379a767b847e8d7624032d7641c7d
Reuben Hernandez <reuben1098@gmail.com>
2020-06-29 18:01:52 -0400

    Week 4 code along work
    - may contain buggy code

---
+++ Week4/week4REST,JS/TourOfHerosREST/src/com/revature/tourofheroes/web/HeroService.java
@@ -0,0 +1,36 @@
+package com.revature.tourofheroes.web;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.revature.tourofheroes.dao.HeroRepoDB;
+import com.revature.tourofheroes.dao.IHeroRepo;
+import com.revature.tourofheroes.models.Hero;
+
+
+@Path("/service") // URL patter from web.xml /heroes/* + /service = /heroes/service. That link will route to this class
+public class HeroService {
+	
+	IHeroRepo heroRepo = new HeroRepoDB();
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addHero(Hero hero) {
+		heroRepo.addHero(hero);
+		return Response.status(201).build();
+	}
+	
+	@GET
+	@Path("/all")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllHeroes() {
+		return Response.ok((ArrayList<Hero>)heroRepo.getAllHeros()).build();
+	}
+}
---
+++ Week4/week4REST,JS/TourOfHerosREST/src/com/revature/tourofheroes/web/MyProps.java
@@ -0,0 +1,7 @@
+package com.revature.tourofheroes.web;
+
+public class MyProps {
+	public static String url = "jdbc:postgresql://ruby.db.elephantsql.com:5432/bqipwjut";
+	public static String username = "bqipwjut";
+	public static String password = "QGvDo6LtedtAo2Eyn2SsCzo3fQ0cHUMD";
+}
